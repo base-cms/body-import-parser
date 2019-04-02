e754a913d63357f5092f9780754f28f6c702e1f2
Jacob Bare <jacob@limit0.io>
2019-04-02 12:48:29 -0500

    Extract deck from body

--- src/rules/penwell/default.js
+++ src/rules/penwell/default.js
@@ -1,13 +1,27 @@
 const cheerio = require('cheerio');
 const adjustHeadings = require('../../utils/adjust-headings');
 
+const extractDeck = ($) => {
+  const className = '.paraStyle_headline_deck';
+  const element = $(className);
+  if (!element.length) return null;
+  const deck = (element.text() || '').trim() || null;
+  element.replaceWith('');
+  return deck;
+};
+
 module.exports = async (body) => {
   const html = (body || '').replace(/\s\s+/g, '');
   const $ = cheerio.load(html);
 
+  const deck = extractDeck($);
+
   adjustHeadings($);
 
   return {
+    extracted: {
+      deck,
+    },
     cleaned: $('body').html(),
     original: body,
   };
