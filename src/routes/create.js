9fa5255709663c35569181b75e25de66203b4fde
Jacob Bare <jacob@limit0.io>
2019-04-02 11:39:32 -0500

    Pass options to rule functions

--- src/routes/create.js
+++ src/routes/create.js
@@ -9,8 +9,16 @@ const createRoute = (app, path, fn) => {
     res.send(`To use, POST an HTML body to ${path}`);
   });
   app.post(path, asyncRoute(async (req, res) => {
-    const { body } = req;
-    const results = await fn(body);
+    const { body, headers } = req;
+    const prefix = 'x-option-';
+    const options = Object.keys(headers)
+      .filter(key => key.indexOf(prefix) === 0)
+      .reduce((o, key) => {
+        const v = headers[key];
+        const k = key.replace(prefix, '');
+        return { ...o, [k]: v };
+      }, {});
+    const results = await fn(body, options);
     return res.json(results);
   }));
 };
