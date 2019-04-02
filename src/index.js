a9a83c834f8f1c7c1caa89b72148f058cde5adee
Jacob Bare <jacob@limit0.io>
2019-04-02 10:32:27 -0500

    Add faux index route

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const app = require('./app');
 const { log } = console;
 const { PORT = 4008 } = process.env;
 
+app.get('/', (req, res) => {
+  res.json({ ping: 'pong' });
+});
+
 app.listen(PORT, () => log(`Service listening on http://0.0.0.0:${PORT}`));
