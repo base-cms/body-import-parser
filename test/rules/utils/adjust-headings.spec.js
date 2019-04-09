26666f8fc87a628eaaf7386fd9e326861d4e7931
Jacob Bare <jacob@limit0.io>
2019-04-09 09:27:41 -0500

    add attributes test

--- test/rules/utils/adjust-headings.spec.js
+++ test/rules/utils/adjust-headings.spec.js
@@ -10,7 +10,7 @@ describe('utils/strip-whitespace', () => {
         <h2>Bar</h2>
         <div>
           <h3>Foo</h3>
-          <h3>Foo</h3>
+          <H3>Foo</H3>
           <h4>Foo</h4>
           <h5>Foo</h5>
           <h6>Foo</h6>
@@ -49,5 +49,26 @@ describe('utils/strip-whitespace', () => {
     expect($('h4').length).to.equal(1, 'Number of <h4> elements');
     expect($('h5').length).to.equal(1, 'Number of <h5> elements');
     expect($('h6').length).to.equal(1, 'Number of <h6> elements');
+  });
+
+  it('should preserve element attributes.', async () => {
+    const body = `
+      <div>
+        <h1 class="foo">Foo</h1>
+        <h2>Bar</h2>
+        <h2 data-foo="bar">Bar</h2>
+        <div>
+          <h3>Foo</h3>
+          <H3>Foo</H3>
+          <h4 class="foo">Foo</h4>
+          <h5 data-foo="bar">Foo</h5>
+          <h6>Foo</h6>
+        </div>
+      </div>
+    `;
+    const $ = cheerio.load(body);
+    adjustHeadings($);
+    expect($('.foo').length).to.equal(2, 'Number of `.foo` elements');
+    expect($('[data-foo="bar"]').length).to.equal(2, 'Number of `[data-foo="bar"]` elements');
   });
 });
