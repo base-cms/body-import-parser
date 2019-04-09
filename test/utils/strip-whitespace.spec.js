61dec65094e513f6664e79a76b6348940767368b
Jacob Bare <jacob@limit0.io>
2019-04-09 15:08:47 -0500

    Update strip whitepsace to handle element text

--- test/utils/strip-whitespace.spec.js
+++ test/utils/strip-whitespace.spec.js
@@ -5,13 +5,15 @@ describe('utils/strip-whitespace', () => {
     const body = `
       <div>
         \t\t<span>Bar   </span>
-        <span>Foo  Bar</span>
+        <span>Foo   <span>
+          Bar      Foo</span>
+  </span>
         <span>Baz Bar</span>\r\n
       </div>
       <p>Hello, World</p>\t
     `;
     const result = stripWhitespace(body);
-    expect(result).to.equal('<div><span>Bar   </span><span>Foo  Bar</span><span>Baz Bar</span></div><p>Hello, World</p>');
+    expect(result).to.equal('<div><span>Bar</span><span>Foo<span>Bar Foo</span></span><span>Baz Bar</span></div><p>Hello, World</p>');
   });
   it('should return an empty string for null values.', async () => {
     expect(stripWhitespace()).to.equal('');
