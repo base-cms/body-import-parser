26fdb6b150c3aaffc37613c05eb3fdbec9ff8a8f
Jacob Bare <jacob@limit0.io>
2019-04-09 08:42:46 -0500

    Additional pennwell tests

--- test/rules/pennwell/default.test.js
+++ test/rules/pennwell/default.test.js
@@ -11,18 +11,52 @@ describe('rules/pennwell/default', () => {
     const result = await rule(body);
     expect(result.html.original).to.equal(body);
   });
-  it('should strip new lines and excessive whitespace.', async () => {
+  it('should strip new lines and excessive whitespace around HMTL elements.', async () => {
     const body = `
       <div>
-        <span>Bar   </span>
+        \t\t<span>Bar   </span>
+        <span>Foo  Bar</span>
+        <span>Baz Bar</span>\r\n
+      </div>
+      <p>Hello, World</p>\t
+    `;
+    const result = await rule(body);
+    expect(result.html.cleaned).to.equal('<div><span>Bar   </span><span>Foo  Bar</span><span>Baz Bar</span></div><p>Hello, World</p>');
+  });
+  it('should remove <form> elements.', async () => {
+    const body = `
+      <div>
+        <form>
+          <input name="foo">
+          <p>Bar</p>
+        </form>
+      </div>
+      <p>Foo</p>
+    `;
+    const result = await rule(body);
+    expect(result.html.cleaned).to.equal('<div></div><p>Foo</p>');
+  });
+  it('should remove <iframe> elements with a src containing pennnet.com.', async () => {
+    const body = `
+      <div>
+        <iframe width="95%" height="40" frameborder="0" scrolling="no" src="https://buyersguide.mae.pennnet.com/Home/SearchBar"></iframe>
+      </div>
+      <iframe src="https://www.youtube.com"></iframe>
+      <p>Foo</p>
+    `;
+    const result = await rule(body);
+    expect(result.html.cleaned).to.equal('<div></div><iframe src="https://www.youtube.com"></iframe><p>Foo</p>');
+  });
+  it('should not convert HTML entities.', async () => {
+    const body = `
+      <div>
+        <p>X > Y</p>
+        <p>&amp;</p>
       </div>
     `;
     const result = await rule(body);
-    expect(result.html.cleaned).to.equal('<div><span>Bar</span></div>');
+    expect(result.html.cleaned).to.equal('<div><p>X > Y</p><p>&</p></div>');
   });
-  it('should remove <form> elements.');
-  it('should remove <iframe> elements with a src containing pennnet.com.');
-  it('should not convert HTML entities.');
   it('should adjust heading elements when an <h1> is present.');
   it('should not adjust heading elements when an <h1> is not present.');
   it('should remove `class` attributes.');
