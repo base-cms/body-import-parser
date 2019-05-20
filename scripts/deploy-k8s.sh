2e424b439bb8434b154bbfece846fb1249c73a4d
Josh Worden <solocommand@gmail.com>
2019-05-20 13:29:10 -0500

    Update deploy-k8s.sh

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -3,4 +3,4 @@ set -e
 
 IMAGE=basecms/body-import-parser:$1
 yarn global add @endeavorb2b/rancher2cli
-r2 dl basecms-service body-import-parser $IMAGE --namespace=imports
+r2 dl basecms-service body-import-parser $IMAGE --namespace=default
