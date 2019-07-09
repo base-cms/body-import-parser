7b2e28b6f71a71422d7451e30b10f6f2cb17ed8f
Josh Worden <solocommand@gmail.com>
2019-07-09 14:23:02 -0500

    Update slack notifications to use environment vars

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+./scripts/deploy-notify-start.sh
 IMAGE=basecms/body-import-parser:$1
 yarn global add @endeavorb2b/rancher2cli
 r2 dl basecms-service body-import-parser $IMAGE --namespace=default
+./scripts/deploy-notify.sh
